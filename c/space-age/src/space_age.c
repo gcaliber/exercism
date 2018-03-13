a81376c2a388f2992797566e55ba9ed03f3e4477
EC2 Default User <ec2-user@ip-172-31-22-114.us-east-2.compute.internal>
2018-03-13 00:23:46 +0000

    first submit of c space-age

--- c/space-age/src/space_age.c
+++ c/space-age/src/space_age.c
@@ -3,12 +3,21 @@
 double convert_planet_age(int planet, long seconds) {
     switch (planet) {
         case EARTH:
-            if 
+            return seconds / EARTH_YEAR;
         case MERCURY:
+            return seconds / MERCURY_YEAR;
         case VENUS:
+            return seconds / VENUS_YEAR;
         case MARS:
+            return seconds / MARS_YEAR;
         case JUPITER:
+            return seconds / JUPITER_YEAR;
         case SATURN:
+            return seconds / SATURN_YEAR;
+        case URANUS:
+            return seconds / URANUS_YEAR;
         case NEPTUNE:
+            return seconds / NEPTUNE_YEAR;
     }
+    return -1;
 }
