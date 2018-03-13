9347b06a6556a3f16c7c07041d7391a45c96f138
Michael Green <mgreen1980@gmail.com>
2018-03-13 11:00:45 -0400

    basic function outline

--- c/meetup/src/meetup.c
+++ c/meetup/src/meetup.c
@@ -1,1 +1,34 @@
 #include "meetup.h"
+#include <string.h>
+
+int meetup_day_of_month(int year, int month, char *day_type, char *day)
+{
+    if (strcmp(day_type, "teenth") == 0) 
+    {
+
+    }
+    else if (strcmp(day_type, "first") == 0)
+    {
+
+    }
+    else if (strcmp(day_type, "second") == 0)
+    {
+
+    }
+    else if (strcmp(day_type, "third") == 0)
+    {
+
+    }
+    else if (strcmp(day_type, "fourth") == 0)
+    {
+
+    }
+    else if (strcmp(day_type, "fifth") == 0)
+    {
+
+    }
+    else if (strcmp(day_type, "last") == 0)
+    {
+
+    }
+}
---
+++ c/meetup/src/meetup.h
@@ -0,0 +1,6 @@
+#ifndef MEETUP_H
+#define MEETUP_H
+
+int meetup_day_of_month(int year, int month, char *day_type, char *day);
+
+#endif
