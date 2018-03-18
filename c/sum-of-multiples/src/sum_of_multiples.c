cfbfd59d7a26fe138a9cd05db682e503b0266466
Michael Green <mgreen1980@gmail.com>
2018-03-18 04:23:46 -0400

    completed c sum-of-multiples

--- c/sum-of-multiples/src/sum_of_multiples.c
+++ c/sum-of-multiples/src/sum_of_multiples.c
@@ -1,49 +1,39 @@
 #include "sum_of_multiples.h"
+#include "../../lib/set_int.h"
 #include <stdlib.h>
 #include <ctype.h>
-#include <math.h>
 
-#define MIN_BUFFER_SIZE 8;
+#include <stdio.h>
 
-struct IntSet {
-    int *buffer;
-    int size;
-    int items;
-};
+#define MIN_BUFFER_SIZE 8
 
-int find (IntSet *set, int n) {
-    for (int i = 0; i < set->items - 1; i++) {
-        if (set->buffer[i] == n)
-            return 1;
-    }
-    return 0;
-}
-
-void add (IntSet *set, int n) {
-    if (find(set, n) == 0) {
-        if (set->items < set->size) {
-            set->buffer[set->items++] = n;
-        }
-        else {
-            set->size = floor(size * 1.5);
-            set->buffer = (int *) realloc(set->buffer, sizeof(int) * set->size);
-            set->buffer[items++] = n;
-        }
-    }
-}
-
+int sum_ints(int *nums, int size);
 
 int sum_of_multiples(const unsigned int *multiples, int size, int max)
 {
-    struct IntSet *set;
-    set->buffer = malloc(sizeof(int) * MIN_BUFFER_SIZE);
-    set->size = MIN_BUFFER_SIZE;
-    set->items = 0;
+    if (!multiples) return 0;
+
+    struct SetInt set;
+    set.buffer = malloc(sizeof(int) * MIN_BUFFER_SIZE);
+    set.size = MIN_BUFFER_SIZE;
+    set.items = 0;
     
     for (int i = 0; i < size; i++) {
         int n = multiples[i];
+        if (n == 0) return 0;
         for (int m = n; m < max; m += n) {
-            add(set, m);
+            set_int_add(&set, m);
         }
     }
+    int sum = sum_ints(set.buffer, set.items);
+    free(set.buffer);
+    return sum;
+}
+
+int sum_ints(int *nums, int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += nums[i];
+    }
+    return sum;
 }
--- c/sum-of-multiples/test/test_sum_of_multiples.c
+++ c/sum-of-multiples/test/test_sum_of_multiples.c
@@ -21,7 +21,7 @@ void test_sum_of_multiples_of_3_and_5_up_to_1(void)
 
 void test_sum_of_multiples_of_3_and_5_up_to_4(void)
 {
-   TEST_IGNORE();               // delete this line to run test
+   //TEST_IGNORE();               // delete this line to run test
    const unsigned int multiples[] = { 3, 5 };
    TEST_ASSERT_EQUAL(3,
                      sum_of_multiples(multiples, NUM_OF_ELEMENTS(multiples),
@@ -30,7 +30,7 @@ void test_sum_of_multiples_of_3_and_5_up_to_4(void)
 
 void test_sum_of_multiples_of_3_and_5_up_to_10(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    const unsigned int multiples[] = { 3, 5 };
    TEST_ASSERT_EQUAL(23,
                      sum_of_multiples(multiples, NUM_OF_ELEMENTS(multiples),
@@ -39,7 +39,7 @@ void test_sum_of_multiples_of_3_and_5_up_to_10(void)
 
 void test_sum_of_multiples_of_3_and_5_up_to_100(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    const unsigned int multiples[] = { 3, 5 };
    TEST_ASSERT_EQUAL(2318,
                      sum_of_multiples(multiples, NUM_OF_ELEMENTS(multiples),
@@ -48,7 +48,7 @@ void test_sum_of_multiples_of_3_and_5_up_to_100(void)
 
 void test_sum_of_multiples_of_3_and_5_up_to_1000(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    const unsigned int multiples[] = { 3, 5 };
    TEST_ASSERT_EQUAL(233168,
                      sum_of_multiples(multiples, NUM_OF_ELEMENTS(multiples),
@@ -57,7 +57,7 @@ void test_sum_of_multiples_of_3_and_5_up_to_1000(void)
 
 void test_sum_of_multiples_of_7_13_and_17_up_to_20(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    const unsigned int multiples[] = { 7, 13, 17 };
    TEST_ASSERT_EQUAL(51,
                      sum_of_multiples(multiples, NUM_OF_ELEMENTS(multiples),
@@ -66,7 +66,7 @@ void test_sum_of_multiples_of_7_13_and_17_up_to_20(void)
 
 void test_sum_of_multiples_of_4_and_6_up_to_15(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    const unsigned int multiples[] = { 4, 6 };
    TEST_ASSERT_EQUAL(30,
                      sum_of_multiples(multiples, NUM_OF_ELEMENTS(multiples),
@@ -75,7 +75,7 @@ void test_sum_of_multiples_of_4_and_6_up_to_15(void)
 
 void test_sum_of_multiples_of_5_6_and_8_up_to_150(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    const unsigned int multiples[] = { 5, 6, 8 };
    TEST_ASSERT_EQUAL(4419,
                      sum_of_multiples(multiples, NUM_OF_ELEMENTS(multiples),
@@ -84,7 +84,7 @@ void test_sum_of_multiples_of_5_6_and_8_up_to_150(void)
 
 void test_sum_of_multiples_of_5_and_25_up_to_51(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    const unsigned int multiples[] = { 5, 25 };
    TEST_ASSERT_EQUAL(275,
                      sum_of_multiples(multiples, NUM_OF_ELEMENTS(multiples),
@@ -93,7 +93,7 @@ void test_sum_of_multiples_of_5_and_25_up_to_51(void)
 
 void test_sum_of_multiples_of_43_and_47_up_to_10000(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    const unsigned int multiples[] = { 43, 47 };
    TEST_ASSERT_EQUAL(2203160,
                      sum_of_multiples(multiples, NUM_OF_ELEMENTS(multiples),
@@ -102,7 +102,7 @@ void test_sum_of_multiples_of_43_and_47_up_to_10000(void)
 
 void test_sum_of_multiples_of_1_up_to_100(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    const unsigned int multiples[] = { 1 };
    TEST_ASSERT_EQUAL(4950,
                      sum_of_multiples(multiples, NUM_OF_ELEMENTS(multiples),
@@ -111,7 +111,7 @@ void test_sum_of_multiples_of_1_up_to_100(void)
 
 void test_sum_of_multiples_of_0_up_to_100(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    const unsigned int multiples[] = { 0 };
    TEST_ASSERT_EQUAL(0,
                      sum_of_multiples(multiples, NUM_OF_ELEMENTS(multiples),
@@ -120,13 +120,13 @@ void test_sum_of_multiples_of_0_up_to_100(void)
 
 void test_no_multiples_given(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    TEST_ASSERT_EQUAL(0, sum_of_multiples(NULL, 0, 10000));
 }
 
 void test_null_ptr_multiple(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    TEST_ASSERT_EQUAL(0, sum_of_multiples(NULL, 3, 10000));
 }
 
