283c021dc29a59ba85108d7bbb53ed7d3564b8e7
Michael Green <mgreen1980@gmail.com>
2018-03-25 22:45:37 +0000

    completed clock

--- c/clock/src/clock.c
+++ c/clock/src/clock.c
@@ -1,4 +1,6 @@
 #include "clock.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void clock(time_text_t time_text, int hour, int minute)
 {
@@ -6,17 +8,22 @@ void clock(time_text_t time_text, int hour, int minute)
     hour %= 24;
     minute %= 60;
     
+    if (minute < 0) {
+        hour -= 1;
+        minute += 60;;
+    }
     if (hour < 0) {
         hour += 24;
     }
-    if (minute < 0) {
-        minute += 60;;
-    }
     
-    
+    sprintf(time_text, "%02d:%02d", hour, minute);
 }
 
 void clock_add(time_text_t time_text, int minute_offset)
 {
-    
+    char *ptr;
+    int hour = (int) strtol(time_text, &ptr, 10);
+    int minute = (int) strtol(&time_text[3], &ptr, 10);
+    minute += minute_offset;
+    clock(time_text, hour, minute);
 }
