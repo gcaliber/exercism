40af35616aa6c8fd232a5cc67a72a7c3d6b5bff8
Michael Green <mgreen1980@gmail.com>
2018-03-31 22:58:57 +0000

    completed collatz-conjecture

---
+++ c/collatz-conjecture/src/collatz_conjecture.c
@@ -0,0 +1,17 @@
+#include "collatz_conjecture.h"
+
+int steps(int start)
+{
+    if (start < 1) return ERROR_VALUE;
+    
+    int n = start;
+    int steps = 0;
+    while (n != 1) {
+        if (n % 2 == 0) 
+            n /= 2; 
+        else 
+            n = 3 * n + 1;
+        steps++;
+    }
+    return steps;
+}
