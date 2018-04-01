4e50b4d86f7e4f3ff85f5f4dea013dfc1078dfd1
Michael Green <mgreen1980@gmail.com>
2018-04-01 19:32:48 +0000

    restructured directories

--- java/two-fer/src/main/java/Twofer.java
+++
@@ -1,8 +0,0 @@
-class Twofer {
-    String twofer(String name) {
-        if (name == null || name.toLowerCase().equals("you")) {
-            return "One for you, one for me.";
-        }
-        return "One for " + name + ", one for me.";
-    }
-}
