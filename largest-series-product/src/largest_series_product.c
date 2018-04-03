1df33b5d952b9f6889de65f53d777a35d1d53660
Michael Green <mgreen1980@gmail.com>
2018-04-03 02:38:16 +0000

    completed largest-series-product

--- largest-series-product/src/largest_series_product.c
+++ largest-series-product/src/largest_series_product.c
@@ -1,6 +1,27 @@
 #include "largest_series_product.h"
+#include <string.h>
+#include <ctype.h>
 
-int largest_series_product(const char *series, length)
+int64_t series_product(const char *series, int length)
 {
-    if (series && length) return 0;
+    int64_t product = 1;
+    for (int i = 0; i < length; i++) {
+        if (!isdigit(series[i])) return -1;
+        product *= series[i] - '0';
+    }
+    return product;
+}
+
+int64_t largest_series_product(const char *series, size_t length)
+{
+    int full_length = strlen(series);
+    if ((int) length > full_length) return -1;
+    
+    int64_t largest = 0;
+    for (int start = 0; start <= (int) (strlen(series) - length); start++) {
+        int64_t product = series_product(&series[start], length);
+        if (product == -1) return -1;
+        if (product > largest) largest = product;
+    }
+    return largest;
 }
--- largest-series-product/src/largest_series_product.h
+++ largest-series-product/src/largest_series_product.h
@@ -1,6 +1,9 @@
 #ifndef LARGEST_SERIES_PRODUCT_H
 #define LARGEST_SERIES_PRODUCT_H
 
-int largest_series_product(const char *series, length);
+#include <stddef.h>
+#include <stdint.h>
+
+int64_t largest_series_product(const char *series, size_t length);
 
 #endif
